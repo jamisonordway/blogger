fc98be19f6075cc7742d79e62ffb2bbfe4f7dd8b
JamisonOrdway <jamisonordway@gmail.com>
2018-05-09 15:14:13 -0600

    Adds show path

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe 'user sees one article' do
   describe 'they link from the articles index' do
-    it 'displays information for one article'
-    article_1 = Article.create(title: 'Best Title', body: 'Uninteresting Article')
+    it 'displays information for one article' do
+    article_1 = Article.create(title: 'Cool Title', body: 'Mediocre Article')
     visit articles_path
     click_link article_1.title
 
     expect(page).to have_content article_1.title
     expect(page).to have_content article_1.body
+    end
   end
 end
